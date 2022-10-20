f78344a9fca6142378650cc9e19e3b963826ab1b
Aytech <ayokunumitoluwani00@gmail.com>
2022-10-20 15:34:13 +0100

    yomt

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,57 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Web3Modal from "web3modal";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
+let web3Modal;
+
+const providerOptions = {
+  
+};
+
+if (typeof window !== "undefined") {
+  web3Modal = new Web3Modal({
+    cacheProvider: false,
+    providerOptions, // required
+  });
+}
+
+
+
 
 export default function Home() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [hasMetamask, setHasMetamask] = useState(false);
+  const [signer, setSigner] = useState(undefined);
+
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      setHasMetamask(true);
+    }
+  });
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const web3ModalProvider = await web3Modal.connect();
+        setIsConnected(true);
+        const provider = new ethers.providers.Web3Provider(web3ModalProvider);
+        setSigner(provider.getSigner());
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnected(false);
+    }
+  }
   return (
     
       <div>
          <div>
         <header>
           <div className="connect-btn-container">
-            <button className="connect-btn">Connect</button>
+            <button className="connect-btn"  onClick={() => connect()}>Connect</button>
           </div>
         </header>
   
